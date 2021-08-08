5f584beff5de8547df7e59f4223a069f0aaba1b0
liuyi <ly1996128@gmail.com>
2021-08-08 22:08:07 +0900

    forms and user accounts

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import (
+    BlogListView, BlogDetailView, BlogCreateView,
+    BlogUpdateView, BlogDeleteView
+)
 
 urlpatterns = [
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delte', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(),
-    name='post_detail'),
+         name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #my app
     'blogs',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
